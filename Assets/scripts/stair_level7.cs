edcbe7f3dc3fac740dc98806dea15f0b1cf0fd73
Hang Ye <piujohanye@gmail.com>
2015-03-16 04:20:50 -0400

    Revert "Revert "3.16""
    This reverts commit 16d577d592d4c02bc4cb9695366f2b0527166a8c.

--- Assets/scripts/stair_level7.cs
+++ Assets/scripts/stair_level7.cs
@@ -6,7 +6,16 @@ public class stair_level7 : MonoBehaviour {
 	private Vector3 state1=new Vector3(-0.05f,-0.105f,0.01f);
 	private Vector3 state2=new Vector3(-0.05f,-0.105f,-0.04f);//final
 
-	public static int state=0;
+	public int state=0;
+
+	public static stair_level7  instance { get; private set; }
+	
+	//When the object awakens, we assign the static variable
+	void Awake() 
+	{
+		instance = this;
+	}
+
 	// Use this for initialization
 	void Start () {
 	
