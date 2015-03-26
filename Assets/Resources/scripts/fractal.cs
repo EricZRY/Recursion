2e1ede799b73404c321dd547838f9e0db2882e11
Hang Ye <piujohanye@gmail.com>
2015-03-26 03:13:29 -0400

    3.25 -working on level select scene

---
+++ Assets/Resources/scripts/fractal.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fractal : MonoBehaviour {
+	public Mesh mesh;
+	public Material material;
+	public int maxDepth;
+	public float childScale;
+	private bool draw=true;
+	private int depth;
+
+	private void Start () {
+		gameObject.AddComponent<MeshFilter>().mesh = mesh;
+		gameObject.AddComponent<MeshRenderer>().material = material;
+
+	}
+
+
+	private void Update(){
+
+		if(draw==true){
+			if (depth < maxDepth) {
+				new GameObject("Fractal Child"+depth).AddComponent<fractal>().
+					Initialize(this, Vector3.up, Quaternion.identity);
+				new GameObject("Fractal Child"+depth).AddComponent<fractal>().
+					Initialize(this, Vector3.right, Quaternion.Euler(0f, 0f, -90f));	
+				new GameObject("Fractal Child"+depth).AddComponent<fractal>().
+					Initialize(this, Vector3.left, Quaternion.Euler(0f, 0f, 90f));
+				//			new GameObject("Fractal Child"+depth).AddComponent<fractal>().
+				//				Initialize(this, Vector3.back, Quaternion.Euler(90f, 180f, 0f));
+				draw=false;
+			}
+		}
+	}
+
+
+	private void Initialize (fractal parent, Vector3 direction, Quaternion orientation) {
+		mesh = parent.mesh;
+		material = parent.material;
+		maxDepth = parent.maxDepth;
+		depth = parent.depth + 1;
+		childScale = parent.childScale;
+
+		transform.parent = parent.transform;
+		transform.localScale = Vector3.one * childScale;
+		transform.localPosition = Vector3.up * (0.5f + 0.5f * childScale);
+		transform.localPosition = direction * (0.5f + 0.5f * childScale);
+		transform.localRotation = orientation;
+
+	}
+}
---
+++ Assets/Resources/scripts/selectLevel.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class selectLevel : MonoBehaviour {
+	private GameObject VRcamera;
+	private GameObject[] levels=new GameObject[8];
+
+	private bool moveRight=false;
+	private bool moveLeft=false;
+	private bool moved = false;
+
+	private Animator rightAni;
+	private Animator leftAni;
+
+	// Use this for initialization
+	void Start () {
+		VRcamera = GameObject.Find ("CenterEyeAnchor");
+		rightAni=GameObject.Find("arrowlowright").GetComponent<Animator>();
+		leftAni=GameObject.Find("arrowlowleft").GetComponent<Animator>();
+
+		for (int i=0; i<8; i++) {
+			levels[i]=GameObject.Find("l"+i);
+		}
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		if (Physics.Raycast (VRcamera.transform.position, VRcamera.transform.TransformDirection (Vector3.forward), out hit)) {
+			if(hit.collider.gameObject==GameObject.Find("right") && moved ==false){
+				moveRight=true;
+				rightAni.SetTrigger("gazed");
+				moved=true;
+			}
+			else if(hit.collider.gameObject==GameObject.Find("left")&& moved ==false){
+				moveLeft=true;
+				leftAni.SetTrigger("gazed");
+				moved=true;
+			}
+
+			for (int i=0; i<8; i++) {
+				if (hit.collider.gameObject==levels[i]){
+					moved=false;
+				}
+			
+			}
+		}
+		else{
+			moved=false;
+		}
+
+		if (moveRight) {
+			//text move right
+		}
+
+
+	}
+}
